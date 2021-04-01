9111c1b6b2a3d904181107c06a25a68710cf8e9e
单志林 <shanzhilin@meprint.com>
2021-04-01 23:20:02 +0800

    fix:修改

--- koa基础/koa-router.js
+++ koa基础/koa-router.js
@@ -18,9 +18,9 @@ router.get('/404', async (ctx, next) => {
     ctx.response.body = '<h1>404 Not Found</h1>'
 })
 
-router.get('/404', async (ctx, next) => {
-    ctx.response.body = '<h1>404 Not Found</h1>'
-})
+// router.get('/404', async (ctx, next) => {
+//     ctx.response.body = '<h1>404 Not Found</h1>'
+// })
 
 
 //读取get传值
